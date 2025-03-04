abca929aa17aefe39bb2c970e5d23096258e82ad
Lypiatt <kevin@lypiatt.com>
2025-03-04 21:36:36 +0000

    split out monitor as a service

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const { Pool } = require('pg');
 const moment = require('moment');
-require('./services/priceMonitor');
 
 const app = express();
 const port = process.env.PORT || 3000;
